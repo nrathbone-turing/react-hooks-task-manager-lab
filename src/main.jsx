6864b7f95f6a168d0ddcf8210acee66572cb223b
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 12:47:32 -0600

    formatting fix for main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App.jsx'
 import { TaskProvider } from './context/TaskContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <TaskProvider>
       <App />
