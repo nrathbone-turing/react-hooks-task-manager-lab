c86de8b662a1acce575b4b383095c677d83b71e8
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 12:37:42 -0600

    Refactor TaskList to not manage its own state and instead pull the tasks from context

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,9 +1,11 @@
 import React, { useContext,useState } from "react";
 import { TaskContext } from "../context/TaskContext";
 
-function TaskList({query}) {
-    const [tasks, setTasks] = useState([]);
-    const filteredTasks = tasks.filter(task =>
+function TaskList({ query }) {
+    
+  const { tasks, toggleComplete } = useContext(TaskContext);
+  
+  const filteredTasks = tasks.filter(task =>
     task.title.toLowerCase().includes(query.toLowerCase())
   );
 
@@ -11,10 +13,13 @@ function TaskList({query}) {
     <ul>
       {filteredTasks.map((task) => (
         <li key={task.id}>
-          <span style={{ textDecoration: task.completed ? "line-through" : "none" }}>
+          <span style={{
+              textDecoration: task.completed ? "line-through" : "none" 
+            }}
+          >
             {task.title}
           </span>
-          <button data-testid={task.id}>
+          <button data-testid={task.id} onClick={() => toggleComplete(task.id)}>
             {task.completed ? "Undo" : "Complete"}
           </button>
         </li>
