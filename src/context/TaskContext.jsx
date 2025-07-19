a3dbca74483f4af0c6dd678fe6b84cf1a24bde51
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-18 23:01:09 -0600

    Add toggleComplete function with logic to update both the web page task list and the db.json file

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -3,4 +3,39 @@ import React, { createContext, useState } from "react";
 export const TaskContext = createContext();
 
 export function TaskProvider({ children }) {
+    const [completed, setCompleted] = useState(false)
+
+    return (
+        <TaskContext.Provider value={{ completed, setCompleted }}>
+        {children}
+        </TaskContext.Provider>
+    );
 }
+
+export function addTask() {
+
+}
+
+export function toggleComplete(id) {
+        
+    fetch(`http://localhost:6001/tasks${id}`, {
+      method:"PATCH",
+      headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ completed: setCompleted })
+    })
+    .then(response => response.json())
+    .then(updatedTask => {
+      setcompleted(tasks =>
+        // Update just the task that was completed using .map() instead of re-fetching all tasks
+        tasks.map(task =>
+          task.id === updatedTask.id ? updatedTask : task,
+          task.completed = true
+        )
+      )
+    })   
+    .catch(err => setError(err.message));
+
+    toggleComplete();
+  };
